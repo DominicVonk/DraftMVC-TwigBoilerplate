23b401ed24223348afee7721b6d40f5651c90c0f
Dominic Vonk <info@dovocode.com>
2017-02-28 18:46:10 +0100

    Fixes == to ===

--- app/models/user.php
+++ app/models/user.php
@@ -2,7 +2,7 @@
 class User extends \DraftMVC\DraftModel {
     public function __set($variable, $value) {
 
-        if ($variable == 'password') {
+        if ($variable === 'password') {
             $this->data['salt'] = Password::generateSalt();
             $this->data[$variable] = Password::create($value, $this->data['salt']);
         } else {
